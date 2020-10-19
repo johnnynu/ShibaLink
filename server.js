317c7dbdf15d44e5ce6c17411469a0dba38a67e5
Johnny <johnny@Johnnys-MBP-2.lan>
2020-10-19 00:03:43 -0700

    "Updated Commit"

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ app.get("/", (req, res) => res.send("API working"));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+<<<<<<< HEAD
 console.log("Hello");
+=======
+>>>>>>> 5600914327df97e88c266c38483e1a365ee0a738
