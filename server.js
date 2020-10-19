eb3a3ffa76e22a065ec4ead19619013d312ea453
Johnny <johnny@Johnnys-MBP-2.lan>
2020-10-18 23:32:28 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API working"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Hi
