9d6ca11a0fc7531cad0df96d46c9fb854ce9b3c9
Johnny <johnny@Johnnys-MBP-2.lan>
2020-10-18 23:45:08 -0700

    Updated Commit

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => res.send("API working"));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// Hi
+console.log("Hello");
